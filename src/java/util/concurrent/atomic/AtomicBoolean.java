56d316bb8bdfd71978af9bf485ff5ba142ebb591
limeng17 <809653838@qq.com>
2020-11-08 22:11:22 +0800

    AtomicBoolean源码分析

--- src/java/util/concurrent/atomic/AtomicBoolean.java
+++ src/java/util/concurrent/atomic/AtomicBoolean.java
@@ -47,6 +47,7 @@ import sun.misc.Unsafe;
  * @since 1.5
  * @author Doug Lea
  */
+// 底层操作1和0实现
 public class AtomicBoolean implements java.io.Serializable {
     private static final long serialVersionUID = 4654671469794556979L;
     // setup to use Unsafe.compareAndSwapInt for updates
